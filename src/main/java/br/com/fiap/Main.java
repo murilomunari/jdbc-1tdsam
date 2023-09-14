d44450d5f471cd81d5584d4205e56b7ee5251c5b
Benefrancis <benefrancis@gmail.com>
2023-09-14 12:03:02 -0300

    Fizemos o findAll, findById, findByName

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,6 +1,8 @@
 package br.com.fiap;
 
+import javax.swing.*;
 import java.sql.*;
+import java.util.Objects;
 
 public class Main {
 
@@ -20,15 +22,77 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        var sql = "SELECT * FROM cliente where UPPER(NM_CLIENTE) like ?";
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            String nome = JOptionPane.showInputDialog( "Nome do cliente" );
+            nome = Objects.nonNull( nome ) ? nome.toUpperCase() : "";
+            preparedStatement.setString( 1, "%" + nome + "%" );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    System.out.println("CLiente: " + resultSet.getLong( "ID_CLIENTE" )
+                            + "- " + resultSet.getString( "NM_CLIENTE" )
+                    );
+                }
+            } else {
+                System.out.println( "Cliente não encontrado com o nome = " + nome );
+            }
+            resultSet.close();  preparedStatement.close(); connection.close();
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar a consulta: \n" + e.getMessage() );
+        }
+    }
 
-      //  findAll();
+    private static void findById() {
+        var sql = "SELECT * FROM cliente where ID_CLIENTE=?";
+        Long id = Long.valueOf( JOptionPane.showInputDialog( "ID do Cliente" ) );
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setLong( 1, id );
 
+            ResultSet resultSet = preparedStatement.executeQuery();
 
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    System.out.println(
+                            "CLiente: " + resultSet.getLong( "ID_CLIENTE" )
+                                    + "- " + resultSet.getString( "NM_CLIENTE" )
+                    );
+                }
+            } else {
+                System.out.println( "Cliente não encontrado com o id = " + id );
+            }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar a consulta: \n" + e.getMessage() );
+        }
+    }
 
+    private static void persist() {
+        var sql = "INSERT INTO cliente (ID_CLIENTE, NM_CLIENTE) VALUES (?,?)";
+        Long id = Long.valueOf( JOptionPane.showInputDialog( "ID" ) );
+        String nome = JOptionPane.showInputDialog( "NOME" );
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setLong( 1, id );
+            preparedStatement.setString( 2, nome );
+            preparedStatement.execute();
+            System.out.println( "Cliente salvo com sucesso!!" );
+            preparedStatement.close();
 
+            connection.close();
+            preparedStatement.close();
+            connection.close();
 
-
-
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar o comando!\n" + e.getMessage() );
+        }
     }
 
     private static void findAll() {
