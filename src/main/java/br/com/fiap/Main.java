b778c347e2e6fb88d4685ab01f720f96a03431a9
murilomunari <129402165+murilomunari@users.noreply.github.com>
2023-09-21 20:09:29 -0300

    terminar na aula amanha

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,55 +1,132 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.entity.Cliente;
-import br.com.fiap.domain.repository.ClienteRepository;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Properties;
+import javax.swing.*;
+import java.sql.*;
+import java.util.Objects;
 
 public class Main {
+
+
     public static Connection getConnection() {
-        Properties prop = new Properties();
-        FileInputStream file = null;
-        String url = null;
-        String pass = null;
-        String user = null;
-        String driver = null;
-        String debugar = null;
+        var url = "jdbc:oracle:thin:@localhost:1521:XE";
+        var url_fiap = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+        String user = "rm94164";
+        String password = "200101";
+        Connection connection = null;
         try {
-
-            file = new FileInputStream( "src/main/resources/application.properties" );
-            prop.load( file );
-
-            url = prop.getProperty( "datasource.url" );
-            user = prop.getProperty( "datasource.username" );
-            pass = prop.getProperty( "datasource.password" );
-            driver = prop.getProperty( "datasource.driver-class-name" );
-            debugar = prop.getProperty( "datasource.debugar" );
-            file.close();
-
-            return DriverManager.getConnection( url, user, pass );
-
-        } catch (FileNotFoundException e) {
-            System.err.println( "Não encontramos o arquivo de configuração: " + e.getMessage() );
-        } catch (IOException e) {
-            System.err.println( "Não foi possível ler o arquivo de configuração: " + e.getMessage() );
+            connection = DriverManager.getConnection( url_fiap, user, password );
         } catch (SQLException e) {
-            System.err.println( "Não foi possível realizar a conexão com o banco de dados: " + e.getMessage() );
+            System.err.println( "Não foi possivel conectar com o banco de dados!" + e.getMessage() );
         }
-        return null;
+        return connection;
     }
-
 
     public static void main(String[] args) {
-        ClienteRepository repo = new ClienteRepository();
-        List<Cliente> clientes = repo.findAll();
-        clientes.forEach( System.out::println );
+        JOptionPane.showMessageDialog( null, "Vamos salvar um cliente no banco de dados" );
+        persist();
+        JOptionPane.showMessageDialog( null, "Listar clientes" );
+        findAll();
+        JOptionPane.showMessageDialog( null, "Consultando cliente por ID" );
+        findById();
+        JOptionPane.showMessageDialog( null, "Listando todos os clientes pelo contúdo do nome" );
+        findByName();
     }
 
+    private static void findByName() {
+        var sql = "SELECT * FROM cliente where UPPER(NM_CLIENTE) like ?";
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            String nome = JOptionPane.showInputDialog( "Nome do cliente" );
+            nome = Objects.nonNull( nome ) ? nome.toUpperCase() : "";
+            preparedStatement.setString( 1, "%" + nome + "%" );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    System.out.println("CLiente: " + resultSet.getLong( "ID_CLIENTE" )
+                            + "- " + resultSet.getString( "NM_CLIENTE" )
+                    );
+                }
+            } else {
+                System.out.println( "Cliente não encontrado com o nome = " + nome );
+            }
+            resultSet.close();  preparedStatement.close(); connection.close();
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar a consulta: \n" + e.getMessage() );
+        }
+    }
+
+    private static void findById() {
+        var sql = "SELECT * FROM cliente where ID_CLIENTE=?";
+        Long id = Long.valueOf( JOptionPane.showInputDialog( "ID do Cliente" ) );
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setLong( 1, id );
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    System.out.println(
+                            "CLiente: " + resultSet.getLong( "ID_CLIENTE" )
+                                    + "- " + resultSet.getString( "NM_CLIENTE" )
+                    );
+                }
+            } else {
+                System.out.println( "Cliente não encontrado com o id = " + id );
+            }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar a consulta: \n" + e.getMessage() );
+        }
+    }
+
+    private static void persist() {
+        var sql = "INSERT INTO cliente (ID_CLIENTE, NM_CLIENTE) VALUES (?,?)";
+        Long id = Long.valueOf( JOptionPane.showInputDialog( "ID" ) );
+        String nome = JOptionPane.showInputDialog( "NOME" );
+        Connection connection = getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setLong( 1, id );
+            preparedStatement.setString( 2, nome );
+            preparedStatement.execute();
+            System.out.println( "Cliente salvo com sucesso!!" );
+            preparedStatement.close();
+
+            connection.close();
+            preparedStatement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível executar o comando!\n" + e.getMessage() );
+        }
+
+    }
+
+    private static void findAll() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery( "SELECT * FROM cliente" );
+
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    Long id = resultSet.getLong( "ID_CLIENTE" );
+                    String nome = resultSet.getString( "NM_CLIENTE" );
+                    System.out.println( "CLIENTE: " + id + " - " + nome );
+                }
+            }
+            System.out.println( "Conexão realizada com sucesso!!!" );
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println( "Não foi possivel consultar os dados!\n" + e.getMessage() );
+        }
+    }
 }
+
